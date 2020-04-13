f542cb62bbf65d8d6e17860f475859c06a4bde61
Jordan Holland <jordan.holland@rea-group.com>
2020-04-13 21:13:18 +1000

    Add kubernetes deployment file

--- src/github/auth.rs
+++ src/github/auth.rs
@@ -7,11 +7,9 @@ use chrono::Utc;
 struct Claims {
     exp: i64, // Required (validate_exp defaults to true in validation). Expiration time
     iat: i64, // Optional. Issued at
-    iss: String         // Optional. Issuer
 }
 
 pub fn authenticate_app() -> Result<std::string::String, Box<dyn std::error::Error>> {
-    let application_id = env::var("APPLICATION_ID")?;
 
     let now = Utc::now().timestamp();
     let ten_minutes_from_now = now + (10 * 60);
@@ -19,10 +17,9 @@ pub fn authenticate_app() -> Result<std::string::String, Box<dyn std::error::Err
     let claim = Claims {
         exp: ten_minutes_from_now,
         iat: now,
-        iss: application_id
     };
 
-    let secret = env::var("PRIVATE_KEY")?;
+    let secret = env::var("GITHUB_APPLICATION_PRIVATE_KEY")?;
 
     let token = encode(&Header::new(Algorithm::RS256), &claim, &EncodingKey::from_rsa_pem(secret.as_bytes())?)?;
 
